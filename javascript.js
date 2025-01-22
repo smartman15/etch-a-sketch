832c7acb31193fccc9ff1e2476c646bb4616009e
khayri <khayriramzy@gmail.com>
2025-01-22 08:12:49 -0300

    Change for loop to add div squares instead of span squares

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ let container = document.querySelector("#container");
 
 
 
-// create 16x16 grid
+// create 16 container divs
 for(let i = 0; i < 16; i++){
-    let box = document.createElement("span");
-    box.textContent = "bing bong";
-    box.classList.add(".box");
+    let box = document.createElement("div");
+    // box.textContent = "bing bong";
+    box.classList.add("square");
     container.appendChild(box);
 }
