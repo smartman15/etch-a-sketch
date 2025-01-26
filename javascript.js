7e07f0b6fe3b81ceb27307b9ee646bd29a1b2d9b
khayri <khayriramzy@gmail.com>
2025-01-26 05:43:29 -0300

    Turn square background color to black when mouseover event is fired

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 let container = document.querySelector("#container");
 
 
-
 // create 16 container divs
 for(let i = 0; i < 256; i++){
     let box = document.createElement("div");
     // box.textContent = "bing bong";
     box.classList.add("square");
+    box.addEventListener("mouseover", () =>{
+        box.style.backgroundColor = "black";
+    })
     container.appendChild(box);
 }
