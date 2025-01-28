86548700dab44419982cce9381b180ebf476927d
khayri <khayriramzy@gmail.com>
2025-01-28 09:03:27 -0300

    Create rows to store squares

--- javascript.js
+++ javascript.js
@@ -2,12 +2,25 @@ let container = document.querySelector(".container");
 
 
 // create 16 container divs
-for(let i = 0; i < 256; i++){
-    let box = document.createElement("div");
-    // box.textContent = "bing bong";
-    box.classList.add("square");
-    box.addEventListener("mouseover", () =>{
-        box.style.backgroundColor = "black";
-    })
-    container.appendChild(box);
+for(let i = 0; i < 16; i++){
+    // create row to store squares
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    // create squares
+    for(let j = 0; j < 16; j++){
+        let box = document.createElement("div");
+        box.classList.add("square");
+
+        // add event listener for whenever mouse goes over square
+        box.addEventListener("mouseover", () =>{
+            box.style.backgroundColor = "black";
+        })
+
+        //store squares into row
+        row.append(box);
+    }
+    
+    //store row into container
+    container.append(row);
 }
